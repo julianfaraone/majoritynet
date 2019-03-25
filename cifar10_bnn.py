72a33bf1f8edf79cfaaabf9abe3bd7440974c401
sfox14 <spcfox2@gmail.com>
2019-03-25 18:48:01 +1100

    replaced dorefa with implementation of bnn

--- cifar10_bnn.py
+++ cifar10_bnn.py
@@ -17,14 +17,14 @@ from tensorpack.tfutils import argscope, get_model_loader, model_utils
 from tensorpack.utils.stats import RatioCounter
 from tensorpack.tfutils.varreplace import remap_variables
 
-from utils import get_dorefa, Conv2D_matmul, Conv2D_muladd
+from utils import get_dorefa, Conv2D_matmul, Conv2D_muladd, bnn
 
 """
 DoreFa-Net ResNet-20 for Cifar10, should get close to 8.75% err (0.27M params)
 https://arxiv.org/abs/1512.03385
 """
-BATCH_SIZE = 1024
 
+BATCH_SIZE = 50 #1024 #50
 BITW = 1
 BITA = 1
 BITG = 32
@@ -38,6 +38,7 @@ class Model(ModelDesc):
     def build_graph(self, image, label):
         image = image / 128.0
 
+        """
         fw, fa, fg = get_dorefa(BITW, BITA, BITG)
 
         def new_get_variable(v):
@@ -54,57 +55,32 @@ class Model(ModelDesc):
 
         def activate(x, name='act'):
             return tf.identity(fa(nonlin(x)), name=name)
+        """
 
-        def resblock(x, channel, stride):
-            def get_stem_full(x):
-                l = Conv2D('c3x3a', x, channel, 3)
-                l = BatchNorm('stembn', l)
-                l = activate(l, name='act_b')
-                l = Conv2D('c3x3b', l, channel, 3)
-                #add_activation_summary(l, ['histogram'], name=l.op.name)
-                #tf.add_to_collection('ACTS', l)
-                return l
+        def get_logits(x):
+            x = bnn.layer('l1', x, 128, filter_size=[3, 3])
+            x = bnn.layer('l2', x, 128, filter_size=[3, 3], pool=([2, 2], [2, 2]))
+            x = bnn.layer('l3', x, 256, filter_size=[3, 3])
+            x = bnn.layer('l4', x, 256, filter_size=[3, 3], pool=([2, 2], [2, 2]))
+            x = bnn.layer('l5', x, 512, filter_size=[3, 3])
+            x = bnn.layer('l6', x, 512, filter_size=[3, 3], pool=([2, 2], [2, 2]))
 
-            channel_mismatch = channel != x.get_shape().as_list()[3]
-            if stride != 1 or channel_mismatch or 'pool1' in x.name:
-                # handling pool1 is to work around an architecture bug in our model
-                if stride != 1 or 'pool1' in x.name:
-                    x = AvgPooling('pool', x, stride, stride)
-                x = BatchNorm('bn', x)
-                x = activate(x, name='act_a')
-                shortcut = Conv2D('shortcut', x, channel, 1)
-                stem = get_stem_full(x)
-            else:
-                shortcut = x
-                x = BatchNorm('bn', x)
-                x = activate(x, name='act_a')
-                stem = get_stem_full(x)
-            return shortcut + stem
+            # fully connected
+            x = bnn.layer('l7', x, 1024, filter_size=[4, 4], padding='VALID')
+            x = bnn.layer('l8', x, 1024)
+            x = bnn.layer('l9', x, 10, activate='none')
+            _y = tf.identity(x)
+            y = tf.reshape(_y, [-1, 10])
+            return y
 
-        def group(name, x, channel, nr_block, stride):
-            with tf.variable_scope(name + 'blk1'):
-                x = resblock(x, channel, stride)
-            for i in range(2, nr_block + 1):
-                with tf.variable_scope(name + 'blk{}'.format(i)):
-                    x = resblock(x, channel, 1)
-            return x
 
-        with remap_variables(new_get_variable), \
-                argscope(BatchNorm, decay=0.9, epsilon=1e-4), \
+
+        #with remap_variables(new_get_variable), \
+        with argscope(BatchNorm, decay=0.9, epsilon=1e-4), \
                 argscope(Conv2D, use_bias=False, nl=tf.identity, 
                     kernel_initializer=tf.variance_scaling_initializer(scale=2.0, mode='fan_out')):
 
-            #acts = tf.get_collection('ACTS')
-
-            l = Conv2D('conv1', image, 16, 3, stride=1, padding='VALID', use_bias=True)
-            l = group('conv2', l, 16, 3, 1)
-            l = group('conv3', l, 32, 3, 2)
-            l = group('conv4', l, 64, 3, 2)
-            l = BatchNorm('lastbn', l)
-            l = nonlin(l)
-            l = GlobalAvgPooling('gap', l)
-            logits = FullyConnected('fct', l, 10, 
-                kernel_initializer=tf.random_normal_initializer(stddev=0.01))
+            logits = get_logits(image)
 
 
         tf.nn.softmax(logits, name='output')
@@ -124,12 +100,25 @@ class Model(ModelDesc):
         add_param_summary(('.*/W', ['histogram']))   # monitor W
         return tf.identity(cost, name='cost')
 
-
+    """
     def optimizer(self):
         lr = tf.get_variable('learning_rate', initializer=args.learning_rate[0], trainable=False)
         opt = tf.train.MomentumOptimizer(lr, 0.9)
         add_moving_summary(lr)
         return opt
+    """
+
+    def optimizer(self):
+        lr = tf.get_variable('learning_rate', initializer=args.learning_rate[0], trainable=False)
+        """
+        lr = tf.train.exponential_decay(
+            learning_rate=lr,
+            global_step=tf.constant(1, dtype=tf.float32),
+            decay_steps=500,
+            decay_rate=dr, staircase=True, name='learning_rate')
+        """
+        add_moving_summary(lr)
+        return tf.train.AdamOptimizer(lr)
 
 
 def get_data(train_or_test):
@@ -159,107 +148,114 @@ def eval_on_cifar10(model, sessinit, ds):
         model=model,
         session_init=sessinit,
         input_names=['input', 'label'],
-        output_names=['wrong_vector', 'conv2blk2/c3x3b/output:0', 'conv2blk2/b_input',
-        'conv2blk2/a_bnorm', 'conv2blk2/c3x3a/output:0', 'conv2blk2/c3x3b/W:0']
+        output_names=['wrong_vector', 'l1/bX', 'l1/bW', 'l2/bX', 'l2/bW', 'l2/xW']
     )
     pred = SimpleDatasetPredictor(pred_config, ds)
     acc1 = RatioCounter()
     for o in pred.get_result():
         batch_size = o[0].shape[0]
         acc1.feed(o[0].sum(), batch_size)
-        actA = o[1]
-        actB = o[2]
-        actC = o[3]
-        actD = o[4]
-        wA = o[5]
+        a1 = o[1]
+        wq1 = o[2]
+        a2 = o[3]
+        wq2 = o[4]
+        w2 = o[5]
     print("Top1 Error: {}".format(acc1.ratio))
-    print actA[0][0][0]
-    print actB[0][0][0]
-    print actC[0][0][0]
-    print actD[0][0][0]
-
-
-    x = wA
-    E = np.sum(np.mean(np.abs(x)))
-    wQ = np.where(np.equal(x, 0), np.ones_like(x), np.sign(x/E))*E
-
-    print actB[0].shape
-    print wQ.shape, x.shape
-    print actA[0].shape
-
     
-    with tf.Session():
-        img = tf.constant(actB[0].reshape(1, 30, 30, 16), dtype=tf.float32)
-        kern = tf.constant(wQ.astype(np.float32), dtype=tf.float32)
-        out = tf.nn.conv2d(img, kern, [1,1,1,1], padding='SAME')
-        ao = out.eval()
+    # quantise w2 and compare with graph quantisation
+    E = np.sum(np.mean(np.abs(w2))) 
+    wQ = np.where(np.equal(w2, 0), np.ones_like(w2), np.sign(w2/E))*E
 
-    print ao[0][0][0]
-    print np.sum(np.abs(ao[0]-actA[0]))
-    print np.allclose(ao[0], actA[0], rtol=1e-2)
+    print wq2.shape, wQ.shape
+    print np.allclose(wQ[0][0][0], wq2[0][0][0], rtol=1e-2)
 
 
 def dump(model, sessinit, ds):
 
     # get model weights
-    weight_list = ['conv2blk1/c3x3a/W:0', 'conv2blk1/c3x3b/W:0',
-                    'conv2blk2/c3x3a/W:0', 'conv2blk2/c3x3b/W:0',
-                    'conv2blk3/c3x3a/W:0', 'conv2blk3/c3x3b/W:0',
-                    'conv3blk1/c3x3a/W:0', 'conv3blk1/c3x3b/W:0',
-                    'conv3blk2/c3x3a/W:0', 'conv3blk2/c3x3b/W:0',
-                    'conv3blk3/c3x3a/W:0', 'conv3blk3/c3x3b/W:0',
-                    'conv4blk1/c3x3a/W:0', 'conv4blk1/c3x3b/W:0',
-                    'conv4blk2/c3x3a/W:0', 'conv4blk2/c3x3b/W:0',
-                    'conv4blk3/c3x3a/W:0', 'conv4blk3/c3x3b/W:0']
+    weight_list = ['l1/bW',
+                    'l2/bW',
+                    'l3/bW',
+                    'l4/bW',
+                    'l5/bW',
+                    'l6/bW',
+                    'l7/bW',
+                    'l8/bW',
+                    'l9/bW']
 
     # get model activations
-    act_list = ['conv2blk1/act_a', 'conv2blk1/act_b',
-                    'conv2blk2/act_a', 'conv2blk2/act_b',
-                    'conv2blk3/act_a', 'conv2blk3/act_b',
-                    'conv3blk1/act_a', 'conv3blk1/act_b',
-                    'conv3blk2/act_a', 'conv3blk2/act_b',
-                    'conv3blk3/act_a', 'conv3blk3/act_b',
-                    'conv4blk1/act_a', 'conv4blk1/act_b',
-                    'conv4blk2/act_a', 'conv4blk2/act_b',
-                    'conv4blk3/act_a', 'conv4blk3/act_b']
+    act_list = ['input']
+
+    # get model batch norm params
+    bn_gamma_list = ['l1/BatchNorm/gamma:0',
+                    'l2/BatchNorm/gamma:0',
+                    'l3/BatchNorm/gamma:0',
+                    'l4/BatchNorm/gamma:0',
+                    'l5/BatchNorm/gamma:0',
+                    'l6/BatchNorm/gamma:0',
+                    'l7/BatchNorm/gamma:0',
+                    'l8/BatchNorm/gamma:0',
+                    'l9/BatchNorm/gamma:0']
+
+    bn_beta_list = ['l1/BatchNorm/beta:0',
+                    'l2/BatchNorm/beta:0',
+                    'l3/BatchNorm/beta:0',
+                    'l4/BatchNorm/beta:0',
+                    'l5/BatchNorm/beta:0',
+                    'l6/BatchNorm/beta:0',
+                    'l7/BatchNorm/beta:0',
+                    'l8/BatchNorm/beta:0',
+                    'l9/BatchNorm/beta:0']
 
     def weight_quant_np(w):
-        x = w
-        E = np.sum(np.mean(np.abs(x)))
-        wQ = np.where(np.equal(x, 0), np.ones_like(x), np.sign(x/E))*E
+        E = np.sum(np.mean(np.abs(w)))
+        wQ = np.where(np.equal(w, 0), np.ones_like(w), np.sign(w/E))*E
         return wQ
 
 
-    def get_node(act, w):
+    def get_node(act, w, bn_g, bn_b):
 
         pred_config = PredictConfig(
             model=model,
             session_init=sessinit,
             input_names=['input', 'label'],
-            output_names=[act, w]
+            output_names=[act, w, bn_g, bn_b, 'label']
         )
         pred = SimpleDatasetPredictor(pred_config, ds)
         acc1 = RatioCounter()
 
         av = []
+        lb = []
         for i, o in enumerate(pred.get_result()):
             av.append(o[0])
             wt = o[1]
+            bt_g = o[2]
+            bt_b = o[3]
+            lb.append(o[4])
             if (i*BATCH_SIZE >= 1000): #limits number of dumped acts
                 break
 
-        data_dict = {act: np.vstack(av), w: weight_quant_np(wt)}
+        data_dict = np.vstack(av), np.hstack(lb), {w: weight_quant_np(wt), bn_g: bt_g, bn_b: bt_b}
         return data_dict
 
-    fd_name = 'resnet20-w{}-a{}-g{}'.format(BITW,BITA,BITG)
+    fd_name = 'cifar10_bnn-w{}-a{}-g{}'.format(BITW,BITA,BITG)
     os.system('mkdir -p dump_log/{}'.format(fd_name))
 
-    for i in range(18):
-        print "fetching: ", act_list[i], weight_list[i]
-        out = get_node(act_list[i], weight_list[i])
-        fname = 'dump_log/{}/node{}.pkl'.format(fd_name, i)
+    for i in range(9): #18
+        print "fetching: ", act_list[0], weight_list[i], bn_gamma_list[i], bn_beta_list[i]
+        img, lbl, out = get_node(act_list[0], weight_list[i], bn_gamma_list[i], bn_beta_list[i])
+        fname = 'dump_log/{}/node{}.pkl'.format(fd_name, i+1)
         with open(fname, 'wb') as fh:
             pickle.dump(out, fh)
+    # dump the image
+    fname = 'dump_log/{}/input.pkl'.format(fd_name)
+    with open(fname, 'wb') as fh:
+        pickle.dump(img, fh)
+
+    fname = 'dump_log/{}/lbl.pkl'.format(fd_name)
+    with open(fname, 'wb') as fh:
+        pickle.dump(lbl, fh)
+
 
 
 if __name__ == '__main__':
@@ -269,8 +265,8 @@ if __name__ == '__main__':
     parser.add_argument('--eval', action='store_true', help='run offline evaluation instead of training')
     parser.add_argument('--dump', action='store_true', help='dump weights and activations')
     parser.add_argument('--dorefa',
-                        help='number of bits for W,A,G, separated by comma. Defaults to \'1,4,32\'',
-                        default='1,4,32')
+                        help='number of bits for W,A,G, separated by comma. Defaults to \'1,1,32\'',
+                        default='1,1,32')
     parser.add_argument('--num_epochs', nargs="+", type=int, default=[150,250,350], help='epoch learning rate schedule')
     parser.add_argument('--learning_rate', nargs="+", type=float, default=[1e-1,1e-2,1e-3], help='learning rate schedule')
     parser.add_argument('--label', help='append string for log_dir', type=str, default='NA')
@@ -295,7 +291,7 @@ if __name__ == '__main__':
         dump(model, get_model_loader(args.load), dataset_test)
 
     else:
-        save_dir = os.path.join('train_log', 'resnet20-w{}-a{}-g{}'.format(BITW,BITA,BITG))
+        save_dir = os.path.join('train_log', 'cifar10_bnn-w{}-a{}-g{}'.format(BITW,BITA,BITG))
         if args.label:
             save_dir = save_dir + "-{}".format(args.label)
         logger.set_logger_dir(save_dir)
@@ -308,9 +304,11 @@ if __name__ == '__main__':
                 MinSaver('validation_error'),
                 InferenceRunner(dataset_test,
                                 [ScalarStats('cost'), ClassificationError('wrong_vector')]),
-                ScheduledHyperParamSetter('learning_rate', 
-                    zip(args.num_epochs[:-1], args.learning_rate[1:]))
+                #ScheduledHyperParamSetter('learning_rate', 
+                #    zip(args.num_epochs[:-1], args.learning_rate[1:]))
                                           #[(1, 0.1), (82, 0.01), (123, 0.001), (300, 0.0002)])
+                HyperParamSetterWithFunc( 'learning_rate', lambda e, x: x*(3e-7/x)**(1.0/args.num_epochs[-1]) )
+
             ],
             max_epoch=args.num_epochs[-1],
             session_init=SaverRestore(args.load) if args.load else None
---
+++ utils/bnn.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import numpy as np
+
+# code
+
+# binary activation function
+def activation(x):
+    x = tf.clip_by_value(x, -1.0, 1.0)
+    return x + tf.stop_gradient(tf.sign(x) - x)
+
+# create weight + bias variables with update op as in BinaryNet
+def weight_bias(shape, binary=True):
+    print(shape)
+    init = np.random.uniform(-1, 1, size=shape) #tf.random_uniform(shape, -1.0, 1.0)
+    #x = tf.Variable(init)
+    x = tf.get_variable('xW', shape, initializer=tf.constant_initializer(init))
+    b = tf.constant(0.1, shape=[shape[-1]])
+
+    if binary:
+        y = tf.get_variable('yW', shape, initializer=tf.constant_initializer(init)) # floating point copy
+
+        coeff = np.float32(1./np.sqrt(1.5/ (np.prod(shape[:-2]) * (shape[-2] + shape[-1]))))
+        print(coeff)
+
+        tmp = y + coeff * (x - y)
+        tmp = tf.clip_by_value(tmp, -1.0, 1.0)
+        tmp = tf.group(x.assign(tmp), y.assign(tmp))
+        tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, tmp)
+
+        x = tf.clip_by_value(x, -1.0, 1.0)
+        xbin = tf.sign(x) * tf.reduce_mean(tf.abs(x)) #, axis=[0, 1, 2]) # maybe uncomment this for training
+        x = x + tf.stop_gradient(xbin - x)
+
+    return x, b #tf.Variable(tf.constant(0.1, shape=[shape[-1]]))
+
+def batch_norm(x, epsilon, decay=0.9):
+    return tf.contrib.layers.batch_norm(x, decay=decay, center=True, scale=True,
+        epsilon=epsilon, updates_collections=None, trainable=True,
+        fused=True)
+
+# a layer in BinaryNet
+def layer(name, x, num_output, filter_size=[1, 1], stride=[1, 1], pool=None, activate='bin',
+          binary=True, norm=True, epsilon=0.0001, padding='SAME'):
+    shape = filter_size + [x.shape[-1].value, num_output]
+
+    with tf.variable_scope(name):
+        W, b = weight_bias(shape, binary)
+
+        Wi = tf.identity(W, name='bW')
+        xi = tf.identity(x, name='bX')
+
+
+        x = tf.nn.conv2d(x, W, strides=[1, stride[0], stride[1], 1], padding=padding) + b
+
+        if activate == 'bin':
+            if pool is not None:
+                x = tf.nn.max_pool(x, ksize=[1, pool[0][0], pool[0][1], 1], strides=[1, pool[-1][0], pool[-1][1], 1], padding='VALID')
+
+            if norm:
+                x = batch_norm(x, epsilon)
+        else:
+            if norm:
+                x = batch_norm(x, epsilon)
+
+            if pool is not None:
+                x = tf.nn.max_pool(x, ksize=[1, pool[0][0], pool[0][1], 1], strides=[1, pool[-1][0], pool[-1][1], 1], padding='VALID')
+
+    if activate == 'bin':
+        return activation(x)
+    elif activate == 'relu':
+        return tf.nn.relu(x)
+
+    assert(activate == 'none')
+    return x
